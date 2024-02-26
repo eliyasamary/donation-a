41a76a208ae54e0784504c5fd5c10802699e725a
Eliya <eliysamaea@gmail.com>
2024-02-26 14:42:18 +0200

    creating "box grid" for displaying buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Header from "./Components/HeaderComponent.jsx";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Box>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
+    <Box className="root">
+      <Header></Header>
+      <Box className="btns-container">
+        <Button variant="contained" className="root-btn">
+          Show All Donasions
+        </Button>
+        <Button variant="contained" className="root-btn">
+          Search Donation
+        </Button>
+        <Button variant="contained" className="root-btn">
+          Update Donation
+        </Button>
+        <Button variant="contained" className="root-btn">
+          New Donation
+        </Button>
+        <Button variant="contained" className="root-btn">
+          Delete Donation
+        </Button>
       </Box>
-    </>
+    </Box>
   );
-}
+};
 
 export default App;
