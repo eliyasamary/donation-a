c5057b0298235459fb7d060cc2ca754af7ede92c
Eliya <eliysamaea@gmail.com>
2024-02-26 14:02:48 +0200

    creating react project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+      </Box>
+    </>
+  );
+}
+
+export default App;
