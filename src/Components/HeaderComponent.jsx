372853ad92969f84daa084bbe334e1dadc618efb
Eliya <eliysamaea@gmail.com>
2024-02-26 14:42:32 +0200

    creating and styling header component

---
+++ src/Components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import Box from "@mui/material/Box";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <>
+      <Box className="header">
+        <h1>Donations App</h1>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
