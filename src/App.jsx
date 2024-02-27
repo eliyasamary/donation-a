6d2cbcbd974b50569fba88aa741c1a036beac841
Eliya <eliysamaea@gmail.com>
2024-02-27 14:24:39 +0200

    making app as layot for the all app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Box from "@mui/material/Box";
-// import Button from "@mui/material/Button";
 import Header from "./Components/HeaderComponent.jsx";
 import Navigation from "./Components/NavComponent.jsx";
-import { getAllDoantions } from "./HTTP/http.js";
 
 const App = () => {
-  const [donations, setDonations] = React.useState([]);
-
-  React.useEffect(() => {
-    getAllDoantions()
-      .then((data) => setDonations(data))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <Box className="root">
       <Header></Header>
       <Navigation></Navigation>
-      <Box className="donations-container">
-        {donations.map((donation) => (
-          <Box key={donation._id} className="donation">
-            <h3>Donor Name: {donation.donorName}</h3>
-            <p>Amount: {donation.amount}</p>
-            <p>Location: {donation.location}</p>
-          </Box>
-        ))}
-      </Box>
+      <Outlet />
     </Box>
   );
 };
