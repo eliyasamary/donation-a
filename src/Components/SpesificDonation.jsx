7ddf0ee9d12272fd898762bcfc6fdedbbf31f978
Eliya <eliysamaea@gmail.com>
2024-02-27 15:38:35 +0200

    modify the component to get a user input for id and retrive the donation match to the id, error display othrwise

--- src/Components/SpesificDonation.jsx
+++ src/Components/SpesificDonation.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { getDoantion } from "../HTTP/http.js";
 
 const SpecificDonation = () => {
   const [id, setId] = React.useState("");
-
   const [donation, setDonation] = React.useState(null);
+  const [notFound, setNotFound] = React.useState(false);
 
   const handleInputChange = (event) => {
     setId(event.target.value);
@@ -15,13 +15,29 @@ const SpecificDonation = () => {
 
   const handleSearch = () => {
     getDoantion(id)
-      .then((data) => setDonation(data))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        if (data) {
+          setDonation(data);
+          setNotFound(false);
+        } else {
+          setDonation(null);
+          setNotFound(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setNotFound(true);
+      });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleSearch();
+    if (id.trim() !== "") {
+      handleSearch();
+    } else {
+      setDonation(null);
+      setNotFound(false);
+    }
   };
 
   return (
@@ -35,27 +51,34 @@ const SpecificDonation = () => {
           gap: "70px",
         }}
       >
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="form-container-row">
           <Box>
             <TextField
+              className="input-field"
               label="Enter Donation ID"
               value={id}
               onChange={handleInputChange}
             />
           </Box>
           <Box>
-            <Button type="submit" variant="contained" color="primary">
+            <Button type="submit" variant="contained" className="form-btn">
               Submit
             </Button>
           </Box>
         </form>
-        {donation && (
+        {donation && !notFound && (
           <Box key={donation._id} className="donation">
             <h3>Donor Name: {donation.donorName}</h3>
             <p>Amount: {donation.amount}</p>
             <p>Location: {donation.location}</p>
           </Box>
         )}
+        {notFound && (
+          <p>
+            s No donation was found matching the provided id. Please ensure the
+            name is entered correctly and try again.
+          </p>
+        )}{" "}
       </Box>
     </Box>
   );
