3f0f4ca63867a93286cb127c399d82a58b5efc98
Eliya <eliysamaea@gmail.com>
2024-02-28 10:07:06 +0200

    adding Edit and Delete functionality

--- src/Components/SpesificDonation.jsx
+++ src/Components/SpesificDonation.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { getDoantion } from "../HTTP/http.js";
+import { getDoantion, updateDoantion, deleteDoantion } from "../HTTP/http.js";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const SpecificDonation = () => {
-  const [id, setId] = React.useState("");
-  const [donation, setDonation] = React.useState(null);
-  const [notFound, setNotFound] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [id, setId] = useState("");
+  const [donation, setDonation] = useState(null);
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDonation, setEditedDonation] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
+  useEffect(() => {
+    if (editedDonation && successMessage) {
+      const timeout = setTimeout(() => {
+        setSuccessMessage("");
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [editedDonation, successMessage]);
 
   const handleInputChange = (event) => {
     setId(event.target.value);
@@ -46,6 +60,49 @@ const SpecificDonation = () => {
     }
   };
 
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+    setEditedDonation(donation);
+  };
+
+  const handleEditChange = (event) => {
+    const { name, value } = event.target;
+    setEditedDonation((prevDonation) => ({
+      ...prevDonation,
+      [name]: value,
+    }));
+  };
+
+  const handleEditSubmit = (event) => {
+    event.preventDefault();
+    updateDoantion(editedDonation)
+      .then(() => {
+        setDonation(editedDonation);
+        setIsEditing(false);
+        setSuccessMessage("Donation updated successfully");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleDelete = () => {
+    setDeleteConfirmation(true);
+  };
+
+  const handleConfirmDelete = () => {
+    deleteDoantion(id)
+      .then(() => {
+        setSuccessMessage("Donation deleted successfully");
+        setDonation(null);
+        setId("");
+      })
+      .catch((err) => console.log(err));
+    setDeleteConfirmation(false);
+  };
+
+  const handleCancelDelete = () => {
+    setDeleteConfirmation(false);
+  };
+
   return (
     <Box className="flex-container-col">
       <h1 className="body-title ">Find Donation By Id</h1>
@@ -74,34 +131,90 @@ const SpecificDonation = () => {
             </Box>
           </form>
           {loading && <ClipLoader color="rgb(46, 42, 165)" />}{" "}
-          {donation && !notFound && !loading && (
-            <Box className="flex-container-col">
-              <p
-                style={{
-                  marginTop: "20px",
-                  marginBottom: "20px",
-                  fontSize: "24px",
-                }}
-              >
-                Donation Found:
-              </p>
-              <Box key={donation._id} className="donation">
-                <h3>Donor Name: {donation.donorName}</h3>
-                <p>Donation ID: {donation._id}</p>
-                <p>Amount: {donation.amount}</p>
-                <p>Location: {donation.location}</p>
+          {donation && !notFound && !loading && !isEditing && (
+            <>
+              <Box className="flex-container-col">
+                <p
+                  style={{
+                    marginTop: "20px",
+                    marginBottom: "20px",
+                    fontSize: "24px",
+                  }}
+                >
+                  Donation Found:
+                </p>
+                <Box key={donation._id} className="donation">
+                  <h3>Donor Name: {donation.donorName}</h3>
+                  <p>Donation ID: {donation._id}</p>
+                  <p>Amount: {donation.amount}</p>
+                  <p>Location: {donation.location}</p>
+                </Box>
+                <Box style={{ display: "flex", gap: "30px" }}>
+                  <Button className="modify-btn" onClick={handleEdit}>
+                    Edit
+                  </Button>
+                  <Button className="modify-btn" onClick={handleDelete}>
+                    Delete
+                  </Button>
+                </Box>
               </Box>
-              <Box style={{ display: "flex", gap: "30px" }}>
-                <Button className="modify-btn">Edit</Button>
-                <Button className="modify-btn">Delete</Button>
-              </Box>
-            </Box>
+            </>
+          )}
+          {isEditing && (
+            <form onSubmit={handleEditSubmit} className="form-container-row">
+              <TextField
+                className="input-field"
+                label="Donor Name"
+                name="donorName"
+                value={editedDonation.donorName}
+                onChange={handleEditChange}
+              />
+              <TextField
+                className="input-field"
+                label="Amount"
+                name="amount"
+                value={editedDonation.amount}
+                onChange={handleEditChange}
+              />
+              <TextField
+                className="input-field"
+                label="Location"
+                name="location"
+                value={editedDonation.location}
+                onChange={handleEditChange}
+              />
+              <Button type="submit" variant="contained" className="form-btn">
+                Save
+              </Button>
+            </form>
           )}
           {notFound && !loading && (
             <p>
               No donation was found matching the provided id. Please ensure the
               name is entered correctly and try again.
             </p>
+          )}
+          {successMessage && (
+            <div className="pop-up">
+              <p>{successMessage}</p>
+            </div>
+          )}
+          {deleteConfirmation && (
+            <div className="pop-up">
+              <p>Are you sure you want to delete this donation?</p>
+              <div className="form-container-row">
+                <div className="btn-wrapper">
+                  <Button onClick={handleConfirmDelete} className="form-btn">
+                    Yes
+                  </Button>
+                </div>
+                <div className="btn-wrapper">
+                  <Button onClick={handleCancelDelete} className="form-btn">
+                    No
+                  </Button>
+                </div>
+              </div>
+            </div>
           )}
         </Box>
       </Box>
