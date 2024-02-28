daba9e116e5e3f821e73a5623f1e74d6ed597d4b
Eliya <eliysamaea@gmail.com>
2024-02-28 18:31:34 +0200

    footer creating

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import Box from "@mui/material/Box";
+import "../CSS/style.css";
+
+const Header = () => {
+  return (
+    <>
+      <Box className="footer">
+        <div>
+          <i className="fa fa-github"></i>
+          <a href="https://github.com/eliyasamary/donation-a" target="_blank">
+            &nbsp;EliyaSamary
+          </a>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
