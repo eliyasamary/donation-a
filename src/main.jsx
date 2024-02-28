7b5b3128f1877e613a4f8d1723bb6eb1dd44c7f0
Eliya <eliysamaea@gmail.com>
2024-02-28 11:04:55 +0200

    adding a route to add new donation

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import SpecificDonation from "./Components/SpesificDonation.jsx";
+import NewDonation from "./Components/NewDonation.jsx";
 import AllDonations from "./Components/AllDonations.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "/:id",
         element: <SpecificDonation />,
+      },
+      {
+        path: "/new",
+        element: <NewDonation />,
       },
     ],
   },
